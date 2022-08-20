74fdbcb2260d6903583912bec194760a9d22e5b3
AkashKarnatak <akashkarnatak.ak47@gmail.com>
2022-08-20 23:49:37 +0530

    Updated startup schema

--- models/user.go
+++ models/user.go
@@ -22,4 +22,20 @@ type Startup struct {
   Description string `json:"description" bson:"Brief about Idea"`
   LinkedinProfile string `json:"linkedinProfile" bson:"Linkedin Profile"`
   Spoc string `json:"spoc" bson:"SPOC"`
+  CurrentScheme string `json:"currentScheme" bson:"Current Scheme"`
+  Sector string `json:"sector" bson:"Sector"`
+  SubSector string `json:"subSector" bson:"Sub sector"`
+  City string `json:"city" bson:"City"`
+  ProductType string `json:"productType" bson:"Product Type"`
+  MarketType string `json:"marketType" bson:"Market type"`
+  CurrentProductStage string `json:"currentProductStage" bson:"Current Product Stage"`
+  HimalayanFocused string `json:"himalayanFocused" bson:"Himalayan focussed"`
+  HimachalBased string `json:"himachalBased" bson:"Himachal based"`
+  StudentStartup string `json:"studentStartup" bson:"Student startup"`
+  IITMandiStartup string `json:"IITMandiStartup" bson:"IIT Mandi Startup"`
+  TrlCurrent string `json:"trlCurrent" bson:"TRL Current"`
+  IncubationMode string `json:"incubationMode" bson:"Mode of Incubation"`
+  Employees string `json:"employees" bson:"Employees"`
+  InvestmentValue string `json:"investmentValue" bson:"Investment Value (L)"`
+  FundsSanctioned string `json:"fundsSanctioned" bson:"Funds sanctioned"`
 }
